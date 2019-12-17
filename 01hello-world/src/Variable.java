f7534b6cc42f6e9b0fe25d8de2c214507678ca38
foochane <fuchengkm@qq.com>
2019-12-17 09:54:25 +0800

    java基础

---
+++ 01hello-world/src/Variable.java
@@ -0,0 +1,8 @@
+public class Variable {
+    public static  void main(String args[]){
+        float f1 = 1f/3f;
+        double d1 = 1d/3d;
+        System.out.println(f1);
+        System.out.println(d1);
+    }
+}
