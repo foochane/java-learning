a1c72a83348af9cd1db8ca1940c352375b649446
foochane <fuchengkm@qq.com>
2019-12-18 10:25:27 +0800

    update'

---
+++ 01hello-world/src/SwichTest.java
@@ -0,0 +1,21 @@
+public class SwichTest {
+    public static void main(String arg[]) {
+        int num = 1;
+        switch (num) {
+
+            case 1:
+                System.out.println(1);
+//                break;
+
+
+            case 2:
+                System.out.println(2);
+//                break;
+            default:
+                System.out.println("不知道");
+                System.out.println();
+        }
+    }
+
+
+}
